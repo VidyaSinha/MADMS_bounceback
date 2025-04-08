7612871c2e989fcabb04b8a5e2bd519c8891441c
VidyaSinha <vidyasinha939@gmail.com>
2025-04-08 10:44:00 +0530

    data table added in 4.2

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx'
+import './index.css'
 
-// Import PrimeReact styles
-import "primereact/resources/themes/lara-light-indigo/theme.css";
-import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
+createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
+ 
 );
